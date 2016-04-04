300daefaae2119487694a8e4f6460b2a06cfe003
ud3p <ud3p@mail.ru>
2016-04-04 16:32:14 +0300

    edit admin requests

--- apps/requests/admin.py
+++ apps/requests/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from .models import Request
 
 class RequestAdmin(admin.ModelAdmin):
-	list_display = ['date', 'method', 'path', 'priority']
+	list_display = ['date', 'method', 'path', 'priority', 'ip_addr']
 
 admin.site.register(Request, RequestAdmin)
